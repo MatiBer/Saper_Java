f39534435020a45942a00bc7536562d57a11db08
MatiBer <01158402@pw.edu.pl>
2022-06-03 03:10:55 +0200

    dziala plansza i podstawowy komunikator

---
+++ src/saper/Saper.java
@@ -0,0 +1,13 @@
+package saper;
+
+
+public class Saper implements Runnable {
+
+    public static void main(String[] args) {
+        SaperWidok okno = new SaperWidok();
+        okno.setVisible(true);
+    }
+    public void run() {
+    }
+}
+
